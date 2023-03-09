17bb8e42af02ac539d2f4bf612755f6ee096f5c8
JoshMagnotta <113945033+JoshMagnotta@users.noreply.github.com>
2023-03-09 16:42:48 -0500

    Update Checkout.java
    Cleaning up comments

--- Checkout.java
+++ Checkout.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package magnottajoshproject1.book;
 
 import java.util.*;
 
-/**
- *
- * @author jmhee
- */
 public class Checkout {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         Books book = new Books();
         CDs cd = new CDs();
